e712d788e9872c5c50dd3de2a04aa002d7af109c
benedit123 <benedit123india@gmail.com>
2023-07-08 01:08:26 +0530

    deploy

---
+++ Bdy.js
@@ -0,0 +1,8 @@
+// const surprise = document.getElementsByClassName('surprise');
+// surprise.addEventListener('click', () => {
+//     window.open('wish.html');
+// })
+function myFunction() {
+
+    window.open('index2.html')
+}
